2b605e10450b4048771acdb6158e97bfb94a802f
QuillshWammy <watariedit008@gmail.com>
2024-04-14 23:22:36 +0900

    Add TestController.java

---
+++ src/main/java/com/quillshwammy/musicdiscovery/TestController.java
@@ -0,0 +1,36 @@
+package com.quillshwammy.musicdiscovery;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/jdbc/sample")
+public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(path="/users", method=RequestMethod.GET)
+    public String index() {
+        List<Map<String,Object>> list;
+        list = jdbcTemplate.queryForList("select * from users");
+        return list.toString();
+    }
+
+    @RequestMapping(path="/users/{id}", method=RequestMethod.GET)
+    public String read(@PathVariable String id) {
+        List<Map<String,Object>> list;
+        list = jdbcTemplate.queryForList("select * from users where id = ?", id);
+        return list.toString();
+    }
+}
