87cffb7cd4cf3f9e23f010388da34b0e79df106c
QuillshWammy <watariedit008@gmail.com>
2024-04-14 17:09:29 +0900

    Initialize gradle project

---
+++ src/main/java/com/quillshwammy/musicdiscovery/MusicdiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.quillshwammy.musicdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicdiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicdiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quillshwammy/musicdiscovery/MusicdiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quillshwammy.musicdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicdiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
