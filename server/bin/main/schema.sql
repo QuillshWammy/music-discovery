146da92dcf823c23f4568c15f6929a2682401867
QuillshWammy <watariedit008@gmail.com>
2024-04-20 22:54:01 +0900

    Delete bin directory

--- server/bin/main/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS songs (
-    id serial PRIMARY KEY,
-    song_name character varying(255),
-    artist_name character varying(255),
-    record_date date DEFAULT CURRENT_DATE
-);
